cb4cc994c32d4262caa7478df8ef4ae966767ea2
zhuyali <495162889@qq.com>
2016-12-01 13:37:50 +0800

    first commit

---
+++ app/src/main/java/com/feedit/julia/feedit_android/MainActivity.java
@@ -0,0 +1,88 @@
+package com.feedit.julia.feedit_android;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.view.KeyEvent;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        init();
+    }
+
+    private void init() {
+
+        webView = (WebView) findViewById(R.id.webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+
+        if (isNetworkAvailable(MainActivity.this)) {
+            settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        } else {
+            settings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        }
+
+        webView.loadUrl("http://10.1.2.124:8080/feedit");
+        webView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+    }
+
+    public boolean isNetworkAvailable(Activity activity) {
+
+        Context context = activity.getApplicationContext();
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (connectivityManager == null) {
+            return false;
+        } else {
+
+            NetworkInfo[] networkInfos = connectivityManager.getAllNetworkInfo();
+
+            if (networkInfos != null && networkInfos.length > 0) {
+                for (int i = 0; i < networkInfos.length; i++) {
+                    if (networkInfos[i].getState() == NetworkInfo.State.CONNECTED) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_BACK) {
+            if(webView.canGoBack()) {
+                webView.goBack();
+                return true;
+            }
+        } else {
+            System.exit(0);
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+}
+
+
